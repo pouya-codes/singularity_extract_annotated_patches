2e8862bc8ea5f842f2eac01f3d9c77684b725514
Pouya Ahmadvand <pouya.ahmadvand@gmail.com>
2023-11-01 11:55:28 -0700

    .

--- extract_annotated_patches/tests/test_extract.py
+++ extract_annotated_patches/tests/test_extract.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import pytest
+from PIL import Image
 import numpy as np
 
 from submodule_utils.metadata.annotation import GroovyAnnotation
@@ -27,6 +28,10 @@ class MockConnection(object):
 
 def test_from_arguments_use_directory_annotation_1(clean_output, mock_data):
     """
+    patches/Tumor/POLE/VOA-1932A/1024/40 1919
+    patches/Stroma/POLE/VOA-1932A/1024/40 131
+
+    TODO: check patch sizes
     """
     slide_path = mock_data['POLE/VOA-1932A']['slide_path']
     slide_coords_location = os.path.join(OUTPUT_DIR, 'slide_coords.json')
@@ -59,7 +64,7 @@ def test_from_arguments_use_directory_annotation_1(clean_output, mock_data):
     patch_files = os.listdir(class_size_to_patch_path['Tumor'][patch_size])
     assert len(patch_files) > 0
     assert len(patch_files) <= int(area['Tumor'] / (1024**2))
-    assert len() == len(patch_files)
+    assert len(extracted_coord_seq) == len(patch_files)
     for patch_file in patch_files:
         patch_name = utils.path_to_filename(patch_file)
         x, y = patch_name.split('_')
@@ -91,6 +96,7 @@ def test_from_arguments_use_directory_annotation_1(clean_output, mock_data):
 @pytest.mark.skip(reason="not finished")
 def test_from_arguments_use_directory_annotation_3(clean_output, mock_data):
     """
+    TODO: check patch sizes
     """
     slide_path = mock_data['POLE/VOA-1932A']['slide_path']
     slide_coords_location = os.path.join(OUTPUT_DIR, 'slide_coords.json')
@@ -121,21 +127,30 @@ def test_from_arguments_use_directory_annotation_3(clean_output, mock_data):
     annotation = mock_data['POLE/VOA-1932A']['annotation']
     area = annotation.get_area()
     """Check Tumor patches"""
-    extracted_coord_seq = cm.get_topleft_coords('Tumor')
-    # patch_files = os.listdir(class_size_to_patch_path['Tumor'][patch_size])
-    # assert len(patch_files) > 0
-    # assert len(patch_files) <= int(area['Tumor'] / (1024**2))
-    # assert len(extracted_coord_seq) == len(patch_files)
-    # for patch_file in patch_files:
-    #     patch_name = utils.path_to_filename(patch_file)
-    #     x, y = patch_name.split('_')
-    #     x = int(x)
-    #     y = int(y)
-    #     assert annotation.points_to_label(np.array([[x, y],
-    #             [x+patch_size, y],
-    #             [x, y+patch_size],
-    #             [x+patch_size, y+patch_size]])) == 'Tumor'
-    #     assert (x, y,) in extracted_coord_seq
+    extracted_coord_seq = list(cm.get_topleft_coords('Tumor'))
+    patch_files_512 = os.listdir(class_size_to_patch_path['Tumor'][512])
+    patch_files_256 = os.listdir(class_size_to_patch_path['Tumor'][256])
+    assert len(patch_files_512) > 0
+    assert len(patch_files_512) <= int(area['Tumor'] / (1024**2))
+    assert len(extracted_coord_seq) == len(patch_files_512)
+    assert len(patch_files_256) == len(patch_files_512)
+    for patch_file_256, patch_file_512 in zip(patch_files_256, patch_files_512):
+        patch_name_512 = utils.path_to_filename(patch_file_512)
+        patch_name_256 = utils.path_to_filename(patch_file_256)
+        assert patch_name_256 == patch_name_512
+        x, y = patch_name_512.split('_')
+        x = int(x)
+        y = int(y)
+        assert annotation.points_to_label(np.array([[x, y],
+                [x+patch_size, y],
+                [x, y+patch_size],
+                [x+patch_size, y+patch_size]])) == 'Tumor'
+        assert (x, y,) in extracted_coord_seq
+        
+        patch_file_512 = os.path.join(class_size_to_patch_path['Tumor'][512], patch_file_512)
+        patch_file_256 = os.path.join(class_size_to_patch_path['Tumor'][256], patch_file_256)
+        patch_512 = Image.open(patch_file_512).convert('RGB')
+        patch_256 = Image.open(patch_file_256).convert('RGB')
 
     # extracted_coord_seq = cm.get_topleft_coords('Stroma')
     # patch_files = os.listdir(class_size_to_patch_path['Stroma'][patch_size])
