9380939487bba346c0b3af7f0bbf7bfde855c251
Pouya Ahmadvand <pouya.ahmadvand@gmail.com>
2023-11-01 11:55:32 -0700

    .

--- extract_annotated_patches/tests/test_run.py
+++ extract_annotated_patches/tests/test_run.py
@@ -87,9 +87,82 @@ def test_from_arguments_use_directory_annotation_1(clean_output, mock_data):
             patch = Image.open(patch_file)
             assert patch.size == (patch_size, patch_size,)
 
+# @pytest.mark.skip(reason="")
+def test_from_arguments_use_directory_annotation_2(clean_output, mock_data):
+    """
+    TODO: test SlideCoordsMetadata more
+    Time: 1998.59s (0:33:18)
+    """
+    slide_coords_location = os.path.join(OUTPUT_DIR, 'slide_coords.json')
+    patch_size = 1024
+    max_slide_patches = 200
+    args_str = f"""
+    from-arguments
+    --patch_location {OUTPUT_PATCH_DIR}
+    use-directory
+    --slide_location {SLIDE_DIR}
+    use-annotation
+    --annotation_location {ANNOTATION_DIR}
+    --slide_coords_location {slide_coords_location}
+    --patch_size {patch_size}
+    --max_slide_patches {max_slide_patches}
+    """
+    parser = extract_annotated_patches.parser.create_parser()
+    config = parser.get_args(args_str.split())
+    ape = AnnotatedPatchesExtractor(config)
+    ape.run()
+
+    """Test SlideCoordsMetadata"""
+    scm = SlideCoordsMetadata.load(slide_coords_location)
+
+    for slide_id in mock_data.keys():
+        _, slide_name = slide_id.split('/')
+        annotation = mock_data[slide_id]['annotation']
+        area = annotation.get_area()
+
+        """Test Tumor patches"""
+        extracted_coord_seq = list(scm.get_slide(slide_name).get_topleft_coords('Tumor'))
+        patch_dir = f"{OUTPUT_PATCH_DIR}/Tumor/{slide_id}/1024/40"
+        patch_files = os.listdir(patch_dir)
+        assert len(patch_files) > 0
+        assert len(patch_files) <= 200
+        assert len(extracted_coord_seq) == len(patch_files)
+        for patch_file in patch_files:
+            patch_name = utils.path_to_filename(patch_file)
+            x, y = patch_name.split('_')
+            x = int(x)
+            y = int(y)
+            assert annotation.points_to_label(np.array([[x, y],
+                    [x+patch_size, y],
+                    [x, y+patch_size],
+                    [x+patch_size, y+patch_size]])) == 'Tumor'
+            assert (x, y,) in extracted_coord_seq
+
+        """Test Stroma Patches"""
+        extracted_coord_seq = list(scm.get_slide(slide_name).get_topleft_coords('Stroma'))
+        patch_dir = f"{OUTPUT_PATCH_DIR}/Stroma/{slide_id}/1024/40"
+        patch_files = os.listdir(patch_dir)
+        assert len(patch_files) > 0
+        assert len(patch_files) <= 200
+        assert len(extracted_coord_seq) == len(patch_files)
+        for patch_file in patch_files:
+            patch_name = utils.path_to_filename(patch_file)
+            x, y = patch_name.split('_')
+            x = int(x)
+            y = int(y)
+            assert annotation.points_to_label(np.array([[x, y],
+                    [x+patch_size, y],
+                    [x, y+patch_size],
+                    [x+patch_size, y+patch_size]])) == 'Stroma'
+            assert (x, y,) in extracted_coord_seq
+
+            patch_file = os.path.join(patch_dir, patch_file)
+            patch = Image.open(patch_file)
+            assert patch.size == (patch_size, patch_size,)
+
 
 # @pytest.mark.skip(reason="not finished")
-def test_from_arguments_use_directory_annotation_2(clean_output, mock_data):
+def test_from_arguments_use_directory_annotation_3(clean_output, mock_data):
     """
     TODO: test SlideCoordsMetadata more
     """
